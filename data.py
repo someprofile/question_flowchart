6667c9725410b96734d4cc6a02a181b66cc7581f
someprofile <iankuzma94@gmail.com>
2025-12-17 18:21:07 -0500

    2. Updated to accept diverging choice paths. Changed x variable to is_answer_option for clarity.

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 
 
 question_answers = {
-    "This is Question A":[["Answer 1", "Answer 2", "Answer 3"],"This is Question B"],
-    "This is Question B":[["Answer 1"],"This is Question C"],
+    "This is Question A":[["Answer 1", "Answer 2"],"This is Question B", "This is Question C"],
+    "This is Question B":[["Answer 1"],"This is Question D"],
     "This is Question C":[["Answer 1"],"This is Question D"],
-    "This is Question D":[["Answer 1"],"This is Question E"],
-    "This is Question E":[["Answer 1"],"This is Question F"],
-    "This is Question F":[["Answer 1"],"This is Question G"],
-    "This is Question G":[["Answer 1"],"This is Question H"],
+    "This is Question D":[["Answer 1", "Answer 2", "Answer 3", "Answer 4"],"This is Question E", "This is Question F", "This is Question G", "This is Question H"],
+    "This is Question E":[["Answer 1"],"This is Question I"],
+    "This is Question F":[["Answer 1"],"This is Question I"],
+    "This is Question G":[["Answer 1"],"This is Question I"],
     "This is Question H":[["Answer 1"],"This is Question I"],
     "This is Question I":[["Answer 1"],"END"],
 }
--- question.py
+++ question.py
@@ -8,19 +8,22 @@ def question_unit(input_question_list, current_question):
 
     print(f"{current_question}\nSelect from these options: \n")
 
+
+    
+
     for item in choice[0]:
         print(f"{item}")
 
-    x = False
-    while x == False:
+    is_answer_option = False
+    while is_answer_option == False:
         answer = input()
         if answer not in choice[0]:
             print("Not an answer. Try again")
         else:
-            x = True
+            is_answer_option = True
 
 
-    return answer, choice[1]
+    return answer, choice[choice[0].index(answer) + 1]
 
 
 
@@ -39,7 +42,9 @@ def flowchart(input_question_list, current_question):
     return
 
 
+'''test = question_unit(question_answers, "This is Question A")
 
+print(test)'''
 
 test = flowchart(question_answers, "This is Question A")
 
